9e58fcf41788e4c3e1338a56468f79ff46f0d78b
Arcane97 <haker_ilnur_97@mail.ru>
2021-08-25 15:26:12 +0500

    Исправил таймаут

--- utils/constants.py
+++ utils/constants.py
@@ -4,4 +4,4 @@ from definitions import ROOT_DIR
 SETTINGS_FILE_NAME = ROOT_DIR + "\\invest_min_trades_settings.json"
 LOG_FILE_NAME = ROOT_DIR + "\\invest_min_trades_log.log"
 NONCE_FILE_NAME = ROOT_DIR + "\\nonce"
-DEFAULT_TIMEOUT = "https://yobit.net/tapi/"
+DEFAULT_TIMEOUT = 3
