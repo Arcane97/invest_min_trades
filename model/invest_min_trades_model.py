8552fa729704b7a945db3b5b33bce71621359341
Arcane97 <haker_ilnur_97@mail.ru>
2021-08-26 14:41:57 +0500

    Добавил старт торгов и проверку трейда

--- model/invest_min_trades_model.py
+++ model/invest_min_trades_model.py
@@ -13,6 +13,7 @@ class InvestMinTradesModel(QObject):
         self.pair = pair
         self.num_trades = num_trades
 
+        self.is_working = False
         self._min_amount = 0.0001
 
         self._yobit_private_api_obj = YobitPrivateAPI(api_key, api_secret.encode(), self.pair, log_name=log_name)
@@ -25,6 +26,31 @@ class InvestMinTradesModel(QObject):
         self._yobit_private_api_obj.pair = self.pair
         self._yobit_public_api_obj = self.pair
 
+    def start_trades(self):
+        self._logger.info('Старт')
+        self.is_working = True
+        last_trade = self._get_last_trade()
+        while self.is_working and self.num_trades > 0:
+            price, quantity = self._get_price_and_quantity()
+            result = self._do_trade(price, quantity)
+
+            if isinstance(result, str):
+                last_trade = self._get_last_trade()
+                self.num_trades -= 1
+                self._logger.info(f'Трейд успешно выполнен. Осталось тредов: {self.num_trades}')
+            else:
+                self._logger.info('При трейде вознилка ошибка, проверяем исполнился ли ордер')
+                if self._check_trade(last_trade):
+                    last_trade = self._get_last_trade()
+                    self.num_trades -= 1
+                    self._logger.info(f'Трейд успешно выполнен. Осталось тредов: {self.num_trades}')
+                else:
+                    self._logger.info('Трейд не выполнен, пытаемя снова')
+
+    def stop_trades(self):
+        self._logger.info('Стоп')
+        self.is_working = False
+
     def _get_price_and_quantity(self):
         glass = self._yobit_public_api_obj.get_yobit_glass()
         amount = 0.0
@@ -32,7 +58,6 @@ class InvestMinTradesModel(QObject):
         glass_index = 0
         while amount < self._min_amount:
             price, glass_quantity = glass[glass_index]
-            print(price, glass_quantity)
             amount = price * glass_quantity
             glass_index += 1
 
@@ -49,8 +74,21 @@ class InvestMinTradesModel(QObject):
     def _do_trade(self, price, quantity):
         return self._yobit_private_api_obj.place_order_buy(price, quantity)
 
+    def _check_trade(self, last_trade):
+        trade_history = self._yobit_private_api_obj.get_trade_history()
+        new_trade = next(iter(trade_history))
+
+        return new_trade != last_trade
+
+    def _get_last_trade(self):
+        trade_history = self._yobit_private_api_obj.get_trade_history()
+        return next(iter(trade_history))
+
 
 if __name__ == "__main__":
-    model = InvestMinTradesModel("", "", "rur_usdt", 1)
+    model = InvestMinTradesModel("", "", "rur_usdt", 3)
     result = model._get_price_and_quantity()
     print('result', result, result[0]*result[1])
+    #
+    # order_id = model._do_trade(*result)
+    # print(order_id)
